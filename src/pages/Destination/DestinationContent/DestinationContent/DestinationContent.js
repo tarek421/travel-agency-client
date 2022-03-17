c77ad2e87b8d7b48425ef6720d80b53c85731688
tarek421 <tarekmahmud740@gmail.com>
2022-03-17 12:49:11 +0600

    end

--- src/pages/Destination/DestinationContent/DestinationContent/DestinationContent.js
+++ src/pages/Destination/DestinationContent/DestinationContent/DestinationContent.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { destinations } from '../../../../Data/Data';
+import Destination from '../../../Home/Destination/Destination';
 import Cart from '../Cart/Cart';
 import './DestinationContent.css';
 import './socialIcon.css';
+import '../../../Home/Destination/Destination.css';
 
 const DestinationContent = ({ item }) => {
-    const { title, description, rating, duration, open, img1, img2, img3 } = item;
+    const { title, description, img1, img2, img3 } = item;
 
     return (
         <div id="destination-content">
             <div className="container">
                 <div className="destination-header">
-                <h3 className='mb-3'>Special {title} Tour</h3>
+                    <h3 className='mb-3'>Special {title} Tour</h3>
                     <div className="row">
                         <div className="col-md-8 col-sm-12">
                             <img className="w-100" src={img1} alt="" />
@@ -31,6 +34,47 @@ const DestinationContent = ({ item }) => {
                         </div>
                     </div>
                 </div>
+
+                <div className="destination-body">
+                    <div className="row my-5 py-5">
+                        <div className="col-md-7 col-sm-12">
+                            <div className="image">
+                                <img src={img2} alt="" />
+                            </div>
+                        </div>
+                        <div className="col-md-5 col-sm-12">
+                            <div className="content">
+                                <h3 className="mb-3">What to See and Do</h3>
+                                <p>{description}</p>
+                                <p className="mt-3">Foam padding in the insoles leather finest quality staple flat slip-on design pointed toe off-duty shoe. Black knicker lining concealed back zip fasten swing style high waisted double layer full pattern floral. Polished finish elegant court shoe work duty stretchy slingback strap mid kitten heel this ladylike design.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="row my-5 pt-5">
+                        <div className="col-md-5 col-sm-12">
+                            <div className="content">
+                                <h3 className="mb-3">How to Get Around</h3>
+                                <p>{description}</p>
+                                <p className="mt-3">See-through delicate embroidered organza blue lining luxury acetate-mix stretch pleat detailing. Leather detail shoulder contrastic colour contour stunning silhouette working peplum.</p>
+                            </div>
+                        </div>
+                        <div className="col-md-7 col-sm-12">
+                            <div className="image">
+                                <img src={img3} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <h3>Related Tours</h3>
+                <hr />
+                <div className="row mt-5">
+                    {
+                        destinations.map(destination => <Destination destination={destination} key={destination.id}></Destination>)
+                    }
+                </div>
+
             </div>
         </div>
     );
